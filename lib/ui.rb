955492a3e8a01484aa719be60f459866233bda24
fcc <fcc.felippe@gmail.com>
2018-03-02 10:57:42 -0300

    implement ui for the game

---
+++ lib/ui.rb
@@ -0,0 +1,51 @@
+class UI
+  attr_accessor :game, :board_class, :player, :mode, :level
+
+  def initialize game, board_class, player_class
+    @game = game
+    @board_class = board_class
+    @player_class = player_class
+  end
+
+  def setup_game
+    @game.board_class = @board_class
+    @game.new_game
+    @mode = ask_for_mode
+    @level = @mode != 'PVP' ? ask_for_level : nil
+    setup_player unless @level.nil?
+  end
+
+  def setup_player
+    @player = @player_class.new @game, @level 
+  end
+
+  def print_state
+    @game.board.print
+  end
+
+  def get_user_input
+    gets.chomp
+  end
+
+  def ask_for_mode
+    puts "select a mode by number:\n1 = 'Player Vs Computer'\n"\
+      "2 = 'Player Vs Player'\n3 = 'Computer Vs Computer'"
+    modes = {1 => 'PVC', 2 => 'PVP', 3 => 'CVC'}
+    mode = get_user_input.to_i
+    ask_for_mode unless (1..3).include? mode
+    return modes[mode]
+  end
+
+  def ask_for_level
+    puts "select a difficult level\n'h' for Hard\n'm' for Medium\n'e' for Easy"
+    levels = {'h' => 'HARD', 'm' => 'MEDIUM', 'e' => 'EASY'}
+    level = get_user_input
+    ask_for_level unless 'hmeHME'.include? level
+    return levels[level.downcase]
+  end
+
+  def ask_for_move
+    puts "Choose your move"
+    get_user_input.to_i
+  end
+end
---
+++ test/test_ui.rb
@@ -0,0 +1,58 @@
+require 'minitest/autorun'
+require 'stringio'
+require_relative '../lib/player'
+require_relative '../lib/game'
+require_relative '../lib/board'
+require_relative '../lib/ui'
+
+class TestUI < Minitest::Test
+  def setup
+    @game = Game.new
+    @ui = UI.new @game, Board, Player
+  end
+
+  def set_input input
+    string_io = StringIO.new
+    string_io.puts input.to_s
+    string_io.rewind
+    $stdin = string_io
+  end
+
+  def clear_stdin
+    $stdin = STDIN
+  end
+
+  def test_ask_for_mode
+    set_input 1
+    mode = @ui.ask_for_mode
+    clear_stdin
+    assert_equal mode, 'PVC'
+    set_input 2
+    assert_equal @ui.ask_for_mode, 'PVP'
+    clear_stdin
+    set_input 3
+    assert_equal @ui.ask_for_mode, 'CVC'
+    clear_stdin
+  end
+
+  def test_ask_for_level
+    set_input 'h'
+    assert_equal @ui.ask_for_level, 'HARD'
+    clear_stdin
+    set_input 'm'
+    assert_equal @ui.ask_for_level, 'MEDIUM'
+    clear_stdin
+    set_input 'e'
+    assert_equal @ui.ask_for_level, 'EASY'
+    clear_stdin
+  end
+
+  def test_ask_for_move
+    (0..9).each do |n|
+      set_input n
+      assert_equal @ui.ask_for_move, n
+      clear_stdin
+    end
+  end
+
+end
